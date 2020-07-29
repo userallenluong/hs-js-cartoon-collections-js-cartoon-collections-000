556137ec1e85659f8e64626082bef5d006b5b9b2
Allen Luong <allenvluong@gmail.com>
2020-07-29 19:51:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 function dwarfRollCall(dwarves) {
+  let dwarvesnumber = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    dwarvesnumber.push(`${i+1}. ${dwarves[i]} `);
+  }
+  return dwarvesnumber.join("")
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  let shoutCaptainPlanet = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    shoutCaptainPlanet.push(`${planeteerCalls[i].toUpperCase()}!`);
+  }
+  return shoutCaptainPlanet;
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
-function findTheCheese (foods) {
-}
+function findTheCheese(foods) {}
